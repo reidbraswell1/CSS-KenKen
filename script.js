8b69ed7e420a7c231cd20d698107a85ca20de997
Reid <reidbras@gmail.com>
2021-08-21 17:04:33 -0500

    Updating dynamic style code

--- script.js
+++ script.js
@@ -1,37 +1,51 @@
-let kenKenRules = [["8×", , "4+"], ["10+", , "4×"], ["1", , , "11+"], ["3+"]];
-
-kenKenRules.forEach(myFunction);
-
-function myFunction(value, rowIndex, array) {
-  for (let colIndex = 0; colIndex < value.length; colIndex++) {
-    console.log(`Row [${rowIndex}] Column [${colIndex}] = ${value[colIndex]}`);
-    if (typeof value[colIndex] != "undefined") {
+let kenKenRules = [
+  ["8×=1,2", , "4+=3,4"],
+  ["10+=5,6,10", , "4×=7,8,11"],
+  ["1=9", , , "11+=12,15,16"],
+  ["3+=14,13"],
+];
+let boxNumber=0;
+//kenKenRules.forEach(myFunction)
+for (let rowIndex = 0; rowIndex < kenKenRules.length; rowIndex++) {
+  for (let colIndex = 0; colIndex < kenKenRules[rowIndex].length; colIndex++) {
+    ++boxNumber;
+    let value = `${kenKenRules[rowIndex][colIndex]}`;
+    let colValue = "undefined";
+    let colStyle = "undefined";
+    if (value != "undefined") {
+      colValue = value.slice(0, value.indexOf("="));
+      colStyle = value.slice(value.indexOf("=") + 1);
+      colStyleArray = colStyle.split(",");
+      console.log(`colValue=${colValue} \n colStyle=${colStyle} \n boxNumber=${boxNumber}`);
+      console.log(`Row [${rowIndex}] Column [${colIndex}] = ${colValue}`);
+    }
+    if (colValue != "undefined") {
       switch (rowIndex) {
         case 0:
           switch (colIndex) {
             case 0:
-              if (typeof value[colIndex] != "undefined") {
+              if (colValue != "undefined") {
                 let box1 = document.getElementById("box1");
-                box1.style.borderLeft="5px solid black";
-                box1.style.borderTop="5px solid black";
-                box1.style.borderBottom="5px solid black";
+                box1.style.borderLeft = "5px solid black";
+                box1.style.borderTop = "5px solid black";
+                box1.style.borderBottom = "5px solid black";
                 let prompt1 = document.getElementById("prompt-1");
-                prompt1.innerText = value[colIndex];
+                prompt1.innerText = colValue;
               }
               break;
             case 1:
-              if (typeof value[colIndex] != "undefined") {
-                document.getElementById("prompt-2").innerText = value[colIndex];
+              if (colValue != "undefined") {
+                document.getElementById("prompt-2").innerText = colValue;
               }
               break;
             case 2:
-              if (typeof value[colIndex] != "undefined") {
-                document.getElementById("prompt-3").innerText = value[colIndex];
+              if (colValue != "undefined") {
+                document.getElementById("prompt-3").innerText = colValue;
               }
               break;
             case 3:
-              if (typeof value[colIndex] != "undefined") {
-                document.getElementById("prompt-4").innerText = value[colIndex];
+              if (colValue != "undefined") {
+                document.getElementById("prompt-4").innerText = colValue;
               }
               break;
           }
@@ -39,23 +53,23 @@ function myFunction(value, rowIndex, array) {
         case 1:
           switch (colIndex) {
             case 0:
-              if (typeof value[colIndex] != "undefined") {
-                document.getElementById("prompt-5").innerText = value[colIndex];
+              if (colValue != "undefined") {
+                document.getElementById("prompt-5").innerText = colValue;
               }
               break;
             case 1:
-              if (typeof value[colIndex] != "undefined") {
-                document.getElementById("prompt-6").innerText = value[colIndex];
+              if (colValue != "undefined") {
+                document.getElementById("prompt-6").innerText = colValue;
               }
               break;
             case 2:
-              if (typeof value[colIndex] != "undefined") {
-                document.getElementById("prompt-7").innerText = value[colIndex];
+              if (colValue != "undefined") {
+                document.getElementById("prompt-7").innerText = colValue;
               }
               break;
             case 3:
-              if (typeof value[colIndex] != "undefined") {
-                document.getElementById("prompt-8").innerText = value[colIndex];
+              if (colValue != "undefined") {
+                document.getElementById("prompt-8").innerText = colValue;
               }
               break;
           }
@@ -63,23 +77,23 @@ function myFunction(value, rowIndex, array) {
         case 2:
           switch (colIndex) {
             case 0:
-              if (typeof value[colIndex] != "undefined") {
-                document.getElementById("prompt-9").innerText = value[colIndex];
+              if (colValue != "undefined") {
+                document.getElementById("prompt-9").innerText = colValue;
               }
               break;
             case 1:
-              if (typeof value[colIndex] != "undefined") {
-                document.getElementById("prompt-10").innerText = value[colIndex];
+              if (colValue != "undefined") {
+                document.getElementById("prompt-10").innerText = colValue;
               }
               break;
             case 2:
-              if (typeof value[colIndex] != "undefined") {
-                document.getElementById("prompt-11").innerText = value[colIndex];
+              if (colValue != "undefined") {
+                document.getElementById("prompt-11").innerText = colValue;
               }
               break;
             case 3:
-              if (typeof value[colIndex] != "undefined") {
-                document.getElementById("prompt-12").innerText = value[colIndex];
+              if (colValue != "undefined") {
+                document.getElementById("prompt-12").innerText = colValue;
               }
               break;
           }
@@ -87,23 +101,23 @@ function myFunction(value, rowIndex, array) {
         case 3:
           switch (colIndex) {
             case 0:
-              if (typeof value[colIndex] != "undefined") {
-                document.getElementById("prompt-13").innerText = value[colIndex];
+              if (colValue != "undefined") {
+                document.getElementById("prompt-13").innerText = colValue;
               }
               break;
             case 1:
-              if (typeof value[colIndex] != "undefined") {
-                document.getElementById("prompt-14").innerText = value[colIndex];
+              if (colValue != "undefined") {
+                document.getElementById("prompt-14").innerText = colValue;
               }
               break;
             case 2:
-              if (typeof value[colIndex] != "undefined") {
-                document.getElementById("prompt-15").innerText = value[colIndex];
+              if (colValue != "undefined") {
+                document.getElementById("prompt-15").innerText = colValue;
               }
               break;
             case 3:
-              if (typeof value[colIndex] != "undefined") {
-                document.getElementById("prompt-16").innerText = value[colIndex];
+              if (colValue != "undefined") {
+                document.getElementById("prompt-16").innerText = colValue;
               }
               break;
           }
@@ -111,4 +125,8 @@ function myFunction(value, rowIndex, array) {
       }
     }
   }
+}
+
+function deriveBoxGridNumber(rowIndex, colIndex) {
+
 }
