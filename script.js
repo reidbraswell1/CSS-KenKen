1b80788d77815e764fb577336f0e60025f1a5f59
reid <reidbras@gmail.com>
2021-08-23 18:48:32 -0500

    Updated script to use new ruleset to apply styling to grid items

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// Style Rules T=Top, R=Right, B=Bottom, L=Left
 let kenKenRules = [
-  ["8×=1,2", , "4+=3,4"],
-  ["10+=5,6,10", , "4×=7,8,11"],
-  ["1=9", , , "11+=12,15,16"],
-  ["3+=14,13"],
+  ["8×=1:T-BL,2:TRB-", , "4+=3:T-BL,4:TRB-"],
+  ["10+=5:T-BL,6:TR--,10:-RBL", , "4×=7:T--L,8:TRB-,11:-RBL"],
+  ["1=9:TRBL", , , "11+=12:TR-L,15:T-BL,16:-RB-"],
+  ["3+=13:T-BL,14:TRB-"],
 ];
 let boxNumber = 0;
 //kenKenRules.forEach(myFunction)
@@ -11,136 +12,29 @@ for (let rowIndex = 0; rowIndex < kenKenRules.length; rowIndex++) {
     ++boxNumber;
     let value = `${kenKenRules[rowIndex][colIndex]}`;
     let colValue = "undefined";
+    let colNum = 0;
     let colStyle = "undefined";
     let colStyleArray = [];
     if (value != "undefined") {
-      colValue = value.slice(0, value.indexOf("="));
+      colValue = getKenKenMathRuleText(value);
+      colNum = getKenKenColumnTextNumber(value);
+      //colValue = value.slice(0, value.indexOf("="));
       colStyle = value.slice(value.indexOf("=") + 1);
       colStyleArray = colStyle.split(",");
-      console.log(
-        `colValue=${colValue} \n colStyle=${colStyle} \n boxNumber=${boxNumber}`
-      );
+      console.log(`colValue=${colValue} \n colStyle=${colStyle} \n boxNumber=${boxNumber}`);
       console.log(`Row [${rowIndex}] Column [${colIndex}] = ${colValue}`);
-      applyGridCageText(colStyleArray[0]*1,colValue);
-      for(let gridCageStyleIndex = 0; gridCageStyleIndex < colStyleArray.length; gridCageStyleIndex++) {
-        let boxNumber = colStyleArray[gridCageStyleIndex];
-        applyGridCageStyling(boxNumber*1, gridCageStyleIndex, colStyleArray);          
+      applyGridCageText(colNum*1, colValue);
+      for (let gridCageStyleIndex = 0; gridCageStyleIndex < colStyleArray.length; gridCageStyleIndex++) {
+        let boxNum = getKenKenColumnTextNumber(colStyleArray[gridCageStyleIndex]);
+        let styleRule = getKenKenStyleRule(colStyleArray, gridCageStyleIndex);
+        applyGridCageStyling(boxNum*1, styleRule);
       }
     }
-    /*
-    if (colValue != "undefined") {
-      switch (rowIndex) {
-        case 0:
-          switch (colIndex) {
-            case 0:
-              if (colValue != "undefined") {
-                let box1 = document.getElementById("box1");
-                box1.style.borderLeft = "5px solid black";
-                box1.style.borderTop = "5px solid black";
-                box1.style.borderBottom = "5px solid black";
-                let prompt1 = document.getElementById("prompt-1");
-                prompt1.innerText = colValue;
-              }
-              break;
-            case 1:
-              if (colValue != "undefined") {
-                document.getElementById("prompt-2").innerText = colValue;
-              }
-              break;
-            case 2:
-              if (colValue != "undefined") {
-                document.getElementById("prompt-3").innerText = colValue;
-              }
-              break;
-            case 3:
-              if (colValue != "undefined") {
-                document.getElementById("prompt-4").innerText = colValue;
-              }
-              break;
-          }
-          break;
-        case 1:
-          switch (colIndex) {
-            case 0:
-              if (colValue != "undefined") {
-                document.getElementById("prompt-5").innerText = colValue;
-              }
-              break;
-            case 1:
-              if (colValue != "undefined") {
-                document.getElementById("prompt-6").innerText = colValue;
-              }
-              break;
-            case 2:
-              if (colValue != "undefined") {
-                document.getElementById("prompt-7").innerText = colValue;
-              }
-              break;
-            case 3:
-              if (colValue != "undefined") {
-                document.getElementById("prompt-8").innerText = colValue;
-              }
-              break;
-          }
-          break;
-        case 2:
-          switch (colIndex) {
-            case 0:
-              if (colValue != "undefined") {
-                document.getElementById("prompt-9").innerText = colValue;
-              }
-              break;
-            case 1:
-              if (colValue != "undefined") {
-                document.getElementById("prompt-10").innerText = colValue;
-              }
-              break;
-            case 2:
-              if (colValue != "undefined") {
-                document.getElementById("prompt-11").innerText = colValue;
-              }
-              break;
-            case 3:
-              if (colValue != "undefined") {
-                document.getElementById("prompt-12").innerText = colValue;
-              }
-              break;
-          }
-          break;
-        case 3:
-          switch (colIndex) {
-            case 0:
-              if (colValue != "undefined") {
-                document.getElementById("prompt-13").innerText = colValue;
-              }
-              break;
-            case 1:
-              if (colValue != "undefined") {
-                document.getElementById("prompt-14").innerText = colValue;
-              }
-              break;
-            case 2:
-              if (colValue != "undefined") {
-                document.getElementById("prompt-15").innerText = colValue;
-              }
-              break;
-            case 3:
-              if (colValue != "undefined") {
-                document.getElementById("prompt-16").innerText = colValue;
-              }
-              break;
-          }
-          break;
-      }
-    }
-    */
   }
 }
 
-
-function applyGridCageText(boxNumber,textValue) {
-
-  switch (boxNumber) {
+function applyGridCageText(boxNumber, textValue) {
+  switch(boxNumber) {
     case 1:
       gridPrompt1 = document.getElementById("prompt-1");
       gridPrompt1.innerText = textValue;
@@ -208,102 +102,111 @@ function applyGridCageText(boxNumber,textValue) {
   }
 }
 
-function applyGridCageStyling(boxNumber,index, array) {
-
-  let topCornerRightGridBox = 4;
-  let bottomCornerRightGridBox = 16;
+function applyGridCageStyling(boxNumber, styleRule) {
 
   let border = "5px solid black";
 
   switch (boxNumber) {
     case 1:
       gridBox1 = document.getElementById("box1");
-      gridBox1.style.borderTop = "5px solid black";
-      gridBox1.style.borderLeft = "5px solid black";
-      if(boxNumber > topCornerRightGridBox) {
-
-      }
-      else {
-        gridBox1.style.borderBottom = border;
-      }
+      applyStyle(gridBox1, styleRule, border);
       break;
     case 2:
       gridBox2 = document.getElementById("box2");
-      gridBox2.style.borderTop = "5px solid black";
-      if(array.length > index) {
-        // Dont highlight the right corner
-      }
-      else {
-        gridBox2.style.borderRight = "5px solid black";
-        gridBox2.style.borderBottom = "5px solid black";
-      }
-      gridBox2.style.borderBottom = "5px solid black";
+      applyStyle(gridBox2, styleRule, border);
       break;
     case 3:
       gridBox3 = document.getElementById("box3");
-      gridBox3.style.borderTop = border;
-      gridBox3.style.borderLeft = border;
-      if(array[index+1] > topCornerRightGridBox) {
-        // Don't highlight the right corner
-      }
-      else {
-        gridBox3.style.borderBottom = border;
-      }
+      applyStyle(gridBox3, styleRule, border);
       break;
     case 4:
       gridBox4 = document.getElementById("box4");
-      gridBox4.style.borderTop = border;
-      gridBox4.style.borderRight = border;
-      if(array.length > index) {
-        // Dont highlight the bottom yet
-      }
-      else {
-        gridBox4.style.borderBottom = border;
-      }
+      applyStyle(gridBox4, styleRule, border);
       break;
     case 5:
       gridBox5 = document.getElementById("box5");
-      gridBox5.style.borderTop = border;
-      gridBox5.style.borderLeft = border;
-      if(array.length > index) {
-        // Dont highlight bottom yet
-      }
-      else {
-        gridBox5.style.borderBottom = border;
-      }
+      applyStyle(gridBox5, styleRule, border);
       break;
     case 6:
       gridBox6 = document.getElementById("box6");
+      applyStyle(gridBox6, styleRule, border);
       break;
     case 7:
       gridBox7 = document.getElementById("box7");
+      applyStyle(gridBox7, styleRule, border);
       break;
     case 8:
       gridBox8 = document.getElementById("box8");
+      applyStyle(gridBox8, styleRule, border);
       break;
     case 9:
       gridBox9 = document.getElementById("box9");
+      applyStyle(gridBox9, styleRule, border);
       break;
     case 10:
       gridBox10 = document.getElementById("box10");
+      applyStyle(gridBox10, styleRule, border);
       break;
     case 11:
       gridBox11 = document.getElementById("box11");
+      applyStyle(gridBox11, styleRule, border);
       break;
     case 12:
       gridBox12 = document.getElementById("box12");
+      applyStyle(gridBox12, styleRule, border);
       break;
     case 13:
       gridBox13 = document.getElementById("box13");
+      applyStyle(gridBox13, styleRule, border);
       break;
     case 14:
       gridBox14 = document.getElementById("box14");
+      applyStyle(gridBox14, styleRule, border);
       break;
     case 15:
       gridBox15 = document.getElementById("box15");
+      applyStyle(gridBox15, styleRule, border);
       break;
     case 16:
       gridBox16 = document.getElementById("box16");
+      applyStyle(gridBox16, styleRule, border);
       break;
+  }
+}
+
+// Return the math rule text i.e. "8x"
+function getKenKenMathRuleText(value) {
+  return value.slice(0, value.indexOf("="));
+}
+
+// Return the column number where the math rule text is located
+function getKenKenColumnTextNumber(value) {
+  let temp1 = value.slice(value.indexOf("=") + 1);
+  let array = temp1.split(",");
+  let colNum = array[0].slice(0,array[0].indexOf(":"));
+  console.log(`getKenKenColumnTextNumber\ncolNum=${colNum}`);
+  return colNum;
+}
+
+// Get the style rule for the KenKen grid box
+function getKenKenStyleRule(array, index) {
+  let temp1 = `${array[index]}`;
+  let styleRule = temp1.slice(temp1.indexOf(":") + 1);
+  return styleRule;
+}
+
+// Apply the style rule to the html object
+function applyStyle(element, styleRule, border) {
+  if(styleRule.slice(0,1) == "T") {
+    element.style.borderTop = border;
+  }
+  if(styleRule.slice(1,2) == "R") {
+    element.style.borderRight = border;
+  }
+  if(styleRule.slice(2,3) == "B") {
+    element.style.borderBottom = border;
+  }
+  if(styleRule.slice(3,4) == "L") {
+    element.style.borderLeft = border;
   }
 }
